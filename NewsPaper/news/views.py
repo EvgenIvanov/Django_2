7e95f5e8329d3cb312d8e8cfb2050b4da618a402
EvgenIvanov <evgen2007@bk.ru>
2021-03-26 22:11:57 +0300

    full news list, sull detail

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,11 +1,18 @@
 # from django.shortcuts import render
 from django.views.generic import ListView, DeleteView
 from .models import Post
+from datetime import datetime
 
 class PostsList(ListView):
     model = Post
     template_name = 'news.html'
     context_object_name = 'news'
+    queryset = Post.objects.order_by('-id')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['date_now'] = datetime.today()
+        return context
 
 class PostDetail(DeleteView):
     model = Post
