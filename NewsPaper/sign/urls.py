bcf20187633701cc6afb8aac2abd38c020075c2f
EvgenIvanov <evgen2007@bk.ru>
2021-04-29 23:14:56 +0300

    add subscribe

--- NewsPaper/sign/urls.py
+++ NewsPaper/sign/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import BaseRegisterView, upgarde_me, UserProfile
+from .views import BaseRegisterView, upgarde_me, UserProfile, subscribe
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('signup/', BaseRegisterView.as_view(template_name='signup.html'), name='signup'),
     path('upgrade/', upgarde_me, name='upgrade'),
+    path('subscribe/', subscribe, name='subscribe'),
     path('profile/', UserProfile.as_view(template_name='user_profile.html'), name='user_profile'),
 ]
--- NewsPaper/sign/views.py
+++ NewsPaper/sign/views.py
@@ -1,9 +1,12 @@
+import news
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, Group
 from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import UserProfile, BaseRegisterForm
+from django.core.mail import send_mail
+from news.models import Category, Subsribers
 
 class BaseRegisterView(CreateView):
     model = User
@@ -17,6 +20,23 @@ def upgarde_me(request):
     if not request.user.groups.filter(name='authors').exists():
         authors_group.user_set.add(user)
     return redirect('/news/')
+
+@login_required
+def subscribe(request):
+    user = request.user
+    qs = request.GET
+    category = Category.objects.get(pk=qs.get('catID'))
+    if not Subsribers.objects.filter(user=user, category=category).exists():
+        Subsribers.objects.create(user=user, category=category)
+        send_mail(
+                subject = 'sublist on the news portal NewsPaper',
+                message = f'Hello {user.username}! You subscribed to the category {category.name} on the news portal NewsPaper.',
+                from_email = 'evgen2007@bk.ru',
+                recipient_list = [f'{user.email}']
+            )
+        return redirect(f'/subscribe/{category.id}')
+    else:
+        return redirect('/')
 
 class UserProfile(LoginRequiredMixin, UpdateView):
     model = User
